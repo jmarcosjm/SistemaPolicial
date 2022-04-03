05cb155ed2be359a15be2eb5e9490688360cb5b5
vitorcrp <66389015+vitorcrp@users.noreply.github.com>
2022-04-03 16:30:06 -0300

    select arma by num registro

--- src/main/java/com/gestao/sistemapolicial/controller/ArmaDAO.java
+++ src/main/java/com/gestao/sistemapolicial/controller/ArmaDAO.java
@@ -3,14 +3,29 @@ package com.gestao.sistemapolicial.controller;
 import com.gestao.sistemapolicial.model.entity.Arma;
 import lombok.Builder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
-@Builder
-@Service
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class ArmaDAO extends AbstractJpaDAO<Arma>{
 
+    EntityManager entityManager;
+
     public ArmaDAO(){
-        //setClazz(Arma.class);
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("armadao");
+        entityManager = entityManagerFactory.createEntityManager();
+        setClazz(Arma.class, entityManager);
+    }
+
+    public Arma findByNumRegistro(String numRegistro){
+        try{
+            return (Arma) entityManager.createQuery("from " + Arma.class.getName() + " where numero_registro = :numRegistro")
+                    .setParameter("numero_registro", numRegistro)
+                    .getSingleResult();
+        }catch(Exception e){
+            return null;
+        }
     }
 
 }
