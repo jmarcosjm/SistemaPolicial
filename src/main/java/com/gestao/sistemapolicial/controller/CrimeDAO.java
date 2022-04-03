d9567c244f8bf3531e4b119e326fb1de139d0567
juau <jmarcosomjm@gmail.com>
2022-04-03 16:29:12 -0300

    fluxo principal

--- src/main/java/com/gestao/sistemapolicial/controller/CrimeDAO.java
+++ src/main/java/com/gestao/sistemapolicial/controller/CrimeDAO.java
@@ -1,7 +1,9 @@
 package com.gestao.sistemapolicial.controller;
 
+import com.gestao.sistemapolicial.model.entity.Arma;
 import com.gestao.sistemapolicial.model.entity.Crime;
 import com.gestao.sistemapolicial.model.entity.Criminoso;
+import com.gestao.sistemapolicial.model.entity.Vitima;
 import lombok.Builder;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -28,6 +30,10 @@ public class CrimeDAO extends AbstractJpaDAO<Crime> {
         var criminosos = crime.getCriminosos();
         var listCriaCriminosos = new ArrayList<Criminoso>();
         var listCriminosos = new ArrayList<Criminoso>();
+        var listCriaArmas = new ArrayList<Arma>();
+        var listArmas = new ArrayList<Arma>();
+        var listCriaVitimas = new ArrayList<Vitima>();
+        var listVitimas = new ArrayList<Vitima>();
         criminosos.forEach(criminoso -> {
             Criminoso criminosoReturn = criminosoDao.findByCpf(criminoso.getCpf());
             if(criminosoReturn == null){
@@ -35,6 +41,9 @@ public class CrimeDAO extends AbstractJpaDAO<Crime> {
             }else{
                 listCriminosos.add(criminosoReturn);
             }
+            criminoso.getArmas().forEach(arma -> {
+
+            });
         });
         create(crime);
         for(Criminoso criminoso : listCriaCriminosos){
